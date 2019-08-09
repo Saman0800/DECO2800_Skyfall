a1912481c46e2668db4afcadc48da8da8f62838d
Ontonator <ontonator@me.com>
2019-08-09 19:13:08 +1000

    Fixed NullPointerException on world generation.

--- src/main/java/deco2800/skyfall/worlds/AbstractWorld.java
+++ src/main/java/deco2800/skyfall/worlds/AbstractWorld.java
@@ -33,6 +33,7 @@ public abstract class AbstractWorld {
 
     protected AbstractWorld() {
     	tiles = new CopyOnWriteArrayList<Tile>();
+        worldGenNodes = new CopyOnWriteArrayList<>();
 
     	generateWorld();
     	generateNeighbours();
