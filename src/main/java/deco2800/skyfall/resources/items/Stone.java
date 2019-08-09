78a35f956872802d86f12dc78599275f2921f5f3
stella_lahanas <s.lahanas@uqconnect.edu.au>
2019-08-09 17:20:04 +1000

    Added Wood and Stone Subclasses with empty methods.

---
+++ src/main/java/deco2800/skyfall/resources/items/Stone.java
@@ -0,0 +1,38 @@
+package deco2800.skyfall.resources.items;
+
+import deco2800.skyfall.resources.Item;
+import deco2800.skyfall.resources.NaturalResources;
+import deco2800.skyfall.util.HexVector;
+import deco2800.skyfall.worlds.Tile;
+
+public class Stone extends NaturalResources implements Item {
+
+    public Stone(String name, Tile position){
+        super(name, position);
+    }
+
+    @Override
+    public String getName() {
+        return null;
+    }
+
+    @Override
+    public Boolean hasHealingPower() {
+        return null;
+    }
+
+    @Override
+    public HexVector getCoords() {
+        return null;
+    }
+
+    @Override
+    public String getSubtype() {
+        return null;
+    }
+
+    @Override
+    public Boolean isCarryable() {
+        return null;
+    }
+}
