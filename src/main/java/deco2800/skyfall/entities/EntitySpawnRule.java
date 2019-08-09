91c6220e88f6a8c4dcb2a600f65c2c9cd4b67d9e
Chalinda Dinusha Rodrigo <chalindarodrigo@gmail.com>
2019-08-09 12:45:52 +1000

    Implemented spawnEntities

--- src/main/java/deco2800/skyfall/entities/EntitySpawnRule.java
+++ src/main/java/deco2800/skyfall/entities/EntitySpawnRule.java
@@ -1,4 +1,59 @@
 package deco2800.skyfall.entities;
 
+/**
+ * Used to set rules about an entity template in EntitySpawnTable
+ * Example rules include probablity, min to max and must eb beside another
+ */
 public class EntitySpawnRule {
+    //chance to spawn in tile, value of zero represents uninitialised
+    private float chance = 0;
+    //minimum number to spawn
+    private int min = 0;
+    //maximum number to spawn
+    private int max = Integer.MAX_VALUE;
+
+    /**
+     * sets spawn rule based on chance
+     * @param chance The likelihood of spawning in a given tile
+     */
+    public EntitySpawnRule(float chance) {
+        setChance(chance);
+    }
+
+    /**
+     * sets spawn rule based on min max
+     * probability will be ~U(min, max) but guaranteed at least min and no more than max
+     * min == max is valid
+     */
+    public EntitySpawnRule(int min, int max) {
+        setChance(-1);
+        setMin(min);
+        setMax(max);
+    }
+
+    //A value of -1 means probability is set by other parameters
+    public float getChance() {
+        return chance;
+    }
+
+    public void setChance(float chance) {
+        this.chance = chance;
+    }
+
+
+    public int getMin() {
+        return min;
+    }
+
+    public void setMin(int min) {
+        this.min = min;
+    }
+
+    public int getMax() {
+        return max;
+    }
+
+    public void setMax(int max) {
+        this.max = max;
+    }
 }
--- src/main/java/deco2800/skyfall/entities/EntitySpawnTable.java
+++ src/main/java/deco2800/skyfall/entities/EntitySpawnTable.java
@@ -4,32 +4,54 @@ import deco2800.skyfall.worlds.AbstractWorld;
 import deco2800.skyfall.worlds.Tile;
 import deco2800.skyfall.managers.GameManager;
 
+import java.util.Collections;
+import java.util.List;
+import java.util.Random;
+
+
 
 /**
- * Simple class containing static methods for placing static entities and NewInstance inheriting entities into the world
- * Does not manage spawn tabling (randomly placing items into the world), requires fixed tile location
+ * Manages spawn tabling (randomly placing items into the world)
+ * Uses EntitySpawnRule for precise spawning
+ * however, simpler methods exist that will handle this for you
  */
-public class EntityPlacing {
+public class EntitySpawnTable {
+
+
     /**
+     * Simple static method for placing static items
      * Takes the given entity and places a deep copy within the world at a given tile
      * @param tile The tile the new entity will occupy
      * @param entity The entity to be deep copied
      * @return The duplicated instance with the new tile position.
+     * See NewInstance to place items
      */
-    public static <T extends StaticEntity> void placeEntity(Tile tile, T entity) {
+    public static <T extends StaticEntity> void placeEntity( T entity, Tile tile) {
         AbstractWorld world = GameManager.get().getWorld();
-        world.addEntity( (T) entity.newInstance(tile) );
+        world.addEntity((T) entity.newInstance(tile));
     }
 
-    /**
-     *Overloads placeEntity(Tile, T extends StaticEntity)
-     * alternative method for creating entities with parameters
-     * Relativly dangerous, there is no compile-time checking on args
-     * Best used
-     */
-    public static <T extends StaticEntity> void placeEntity(Tile tile, Object... args) {
-        AbstractWorld world = GameManager.get().getWorld();
-        world.addEntity(  new T<>(args));
-    }
+    public static <T extends StaticEntity> void spawnEnities(T entity, EntitySpawnRule rule) {
+        int max = rule.getMax();
+        int min = rule.getMin();
+        double chance = rule.getChance() < 0 ? rule.getChance() : 1 / (double)( max - min );
 
+        AbstractWorld world = GameManager.get().getWorld();
+        Random r = new Random();
+
+        List<Tile> tiles = world.getTileMap();
+        Collections.shuffle(tiles, r);
+
+        int placed = 0;
+
+        //continue iterating over tiles while rule needs to be satisfied
+        for (int i = 0; (i < tiles.size() || placed <= min) && placed <= max; i = (++i)%tiles.size()) {
+            Tile tile = tiles.get(i);
+            if (tile != null && r.nextDouble() < chance) {
+                placeEntity(entity, tile);
+            }
+        }
+
+
+    }
 }
