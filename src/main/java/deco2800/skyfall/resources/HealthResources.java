bd5c3d0859100839a98c0042ed4ea2853fd2a4d8
45109762 <zheng0451088698@gmail.com>
2019-08-09 20:56:48 +1000

    Added 4 new methods into HealthResources class, and other methods are similar to NaturalResources class, the boolean value of hasHealingPower and subtype are changed

--- src/main/java/deco2800/skyfall/resources/HealthResources.java
+++ src/main/java/deco2800/skyfall/resources/HealthResources.java
@@ -1,5 +1,123 @@
 package deco2800.skyfall.resources;
 
 public abstract class HealthResources implements Item {
+    // can the item be stored in the inventory
+    private Boolean carryable;
+    // the name of the item e.g. food, poison
+    private String name;
+    //impact the player's health or not
+    private Boolean hasHealingPower;
+    // the name of the subtype the item belongs to
+    private String subtype;
+    // the co-ordinates of the tile the item has been placed on
+    private HexVector position;
+    //How many amount of healing power could be recovered
+    private Int AmountOfHealingPower;
+    //How many amount of healing will be deducted if have a poison
+    private Int HealingDeducted;
+    //if and only if the items deduct the HP of player
+    private Boolean notHealingPower;
+    //Items could change or not e.g. coins, items
+    private Boolean exchangeable;
 
+
+
+    /**
+     * Creates a new Health Resource with the given name
+     * @param name the identifying name of the Health Resource
+     * @param position the tile which the item has been placed on
+     */
+    public HealthResources(String name, Tile position) {
+        this.name = name;
+        this.carryable = true;
+        this.subtype = "Health Resource";
+        this.hasHealingPower = true;
+        this.notHealingPower=false;
+        this.exchangeable=true;
+        this.position = position.getCoordinates();
+    }
+
+
+    /**
+     * Returns the name of the health resource
+     * @return The name of the health resource
+     */
+    @Override
+    public String getName() {
+        return name;
+    }
+
+
+    /**
+     * Returns whether or not the item can be stored in the inventory
+     * @return True if the item can be added to the inventory, false
+     * if it is consumed immediately
+     */
+    @Override
+    public Boolean getCarryable() {
+        return carryable;
+    }
+
+    /**
+     * Returns the subtype which the item belongs to.
+     * @return The subtype which the item belongs to.
+     */
+    @Override
+    public String getSubtype() {
+        return subtype;
+    }
+
+    /**
+     * Returns whether or not the item impacts the player's health
+     * @return True if the item impacts on the player's health, false otherwise
+     */
+    @Override
+    public Boolean getHasHealingPower() {
+        return hasHealingPower;
+    }
+
+    /**
+     * Returns whether or not the item could deduct the HP of players
+     * @return True if the item deduct the player's health, false otherwise
+     */
+    @Override
+    public Boolean getNotHealingPower() {
+        return notHealingPower;
+    }
+
+    /**
+     * Returns the integer of healing power
+     * @return the integer of healing power
+     */
+    public Int AmountOfHealingPower(Int AmountOfRecoverHP){
+        AmountOfHealingPower=AmountOfRecoverHP;
+        return AmountOfHealingPower;
+    }
+
+    /**
+     * Returns the integer of deducting healing power
+     * @return the integer of deducting healing power
+     */
+    public Int HealingDeducted(int AmountOfDeducted){
+        HealingDeducted=AmountOfDeducted;
+        return HealingDeducted;
+    }
+
+    /**
+     * Returns whether or not the item could be exchanged
+     * @return True if the item could be exhanged, false otherwise
+     */
+    @Override
+    public Boolean getExchangeable() {
+        return exchangeable;
+    }
+
+    /**
+     * Returns the co-ordinates of the tile the item is on.
+     * @return the co-ordinates of the tile the item is on.
+     */
+    @Override
+    public HexVector getCoords() {
+        return position;
+    }
 }
