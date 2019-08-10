8befa6f6f2b29bf27183960d9d82b728ab838f1e
stella_lahanas <s.lahanas@uqconnect.edu.au>
2019-08-10 11:57:53 +1000

    Created a Natural Resources and Wood Test File.

---
+++ src/test/java/deco2800/skyfall/resources/NaturalResourcesTest.java
@@ -0,0 +1,46 @@
+package deco2800.skyfall.resources;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class NaturalResourcesTest extends NaturalResources {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void getName1() {
+    }
+
+    @Test
+    public void isCarryable1() {
+    }
+
+    @Test
+    public void getSubtype1() {
+    }
+
+    @Test
+    public void hasHealingPower1() {
+    }
+
+    @Test
+    public void getCoords1() {
+    }
+
+    @Test
+    public void getExchangeable1() {
+    }
+
+    @Test
+    public void toString1() {
+    }
+}
---
+++ src/test/java/deco2800/skyfall/resources/items/WoodTest.java
@@ -0,0 +1,45 @@
+package deco2800.skyfall.resources.items;
+
+import deco2800.skyfall.resources.NaturalResources;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class WoodTest extends NaturalResources {
+
+    @Test
+    public void getName1() {
+    }
+
+    @Test
+    public void isCarryable1() {
+    }
+
+    @Test
+    public void getSubtype1() {
+    }
+
+    @Test
+    public void hasHealingPower1() {
+    }
+
+    @Test
+    public void getCoords1() {
+    }
+
+    @Test
+    public void getExchangeable1() {
+    }
+
+    @Test
+    public void toString1() {
+    }
+
+    @Test
+    public void getColour() {
+    }
+
+    @Test
+    public void getBiome() {
+    }
+}
