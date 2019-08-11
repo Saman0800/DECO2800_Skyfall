9d020fd61b65febb6b8479689103d152150a087d
45109762 <zheng0451088698@gmail.com>
2019-08-11 17:31:26 +1000

    Fixed the errors in NaturalResoures

--- src/main/java/deco2800/skyfall/resources/NaturalResources.java
+++ src/main/java/deco2800/skyfall/resources/NaturalResources.java
@@ -1,21 +1,22 @@
 package deco2800.skyfall.resources;
 
+import deco2800.skyfall.resources.items.Wood;
 import deco2800.skyfall.util.HexVector;
 import deco2800.skyfall.worlds.Tile;
 
 /**
  * An abstract class representing a Natural Resource item
  */
-public abstract class NaturalResources implements Item {
+public class NaturalResources implements Item {
 
     // the name of the item e.g. wood, stone
-    private String name;
+    public String name;
 
     // can the item be stored in the inventory
     private Boolean carryable;
 
     // the name of the subtype the item belongs to
-    private String subtype;
+    public String subtype;
 
     // does the item impact the player's health
     private Boolean hasHealingPower;
@@ -23,17 +24,34 @@ public abstract class NaturalResources implements Item {
     // the co-ordinates of the tile the item has been placed on
     private HexVector position;
 
-    /**
-     * Creates a new Natural Resource with the given name
-     * @param name the identifying name of the Natural Resource
-     * @param position the tile which the item has been placed on
+    // determines whether or not the resource can be traded
+    private Boolean exchangeable;
+
+    /***
+     * Creates a default natural resource
      */
-    public NaturalResources(String name, Tile position){
-        this.name = name;
+    public NaturalResources(){
+        //default constructor added for building inventory
+        this.name = null;
+        this.carryable = true;
+        this.subtype = "Natural Resource";
+        this.hasHealingPower = false;
+        this.exchangeable = true;
+
+    }
+
+
+    /**
+     * Creates a new Natural Resource with the given name and position
+     * * @param position the tile which the item has been placed on
+     */
+    public NaturalResources(Tile position){
+        this.name = null;
         this.carryable = true;
         this.subtype = "Natural Resource";
         this.hasHealingPower = false;
         this.position = position.getCoordinates();
+        this.exchangeable = true;
 
     }
 
@@ -45,6 +63,7 @@ public abstract class NaturalResources implements Item {
     public String getName() {
         return name;
     }
+
 
     /**
      * Returns whether or not the item can be stored in the inventory
@@ -82,4 +101,35 @@ public abstract class NaturalResources implements Item {
     public HexVector getCoords() {
         return position;
     }
+
+    @Override
+    public Boolean getExchangeable() {
+        return exchangeable;
+    }
+
+    /**
+     * Creates a string representation of the natural resource in the format:
+     *
+     * <p>'{Nautral Resource}:{Name}'
+     *
+     * <p>without surrounding quotes and with {natural resource} replaced by
+     * the subtype of the item and {name} replaced with the item name
+     * For example:
+     *
+     * <p>Natural Resource:Wood
+     *
+     * @return A string representation of the natural resource.
+     */
+    @Override
+    public String toString() {
+        return "" + subtype + ":" + name;
+    }
+
+
+    public static void main(String[] args) {
+        NaturalResources n = new Wood();
+        System.out.println(n.getName());
+        System.out.println(n.getSubtype());
+    }
+
 }
