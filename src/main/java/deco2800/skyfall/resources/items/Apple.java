e4f69f1abd40a4de51f3517e40b426b6b6f1b693
45109762 <zheng0451088698@gmail.com>
2019-08-11 22:42:08 +1000

    Added apple into item

---
+++ src/main/java/deco2800/skyfall/resources/items/Apple.java
@@ -0,0 +1,69 @@
+package deco2800.skyfall.resources.items;
+
+import deco2800.skyfall.resources.Item;
+import deco2800.skyfall.resources.HealthResources;
+import deco2800.skyfall.util.HexVector;
+import deco2800.skyfall.worlds.Tile;
+
+public abstract class Apple extends HealthResources implements Item {
+    // the name of the item
+    private String name;
+
+    // can the item be stored in the inventory
+    private Boolean carryable;
+
+    // the name of the subtype the item belongs to
+    private String subtype;
+
+    // does the item impact the player's health
+    private Boolean hasHealingPower;
+
+    // the co-ordinates of the tile the item has been placed on
+    private HexVector position;
+
+    // determines whether or not the resource can be traded
+    private Boolean exchangeable;
+
+    // the biome the sand is in (will change to different type in future?)
+    private String biome;
+
+    //How many amount of healing power could be recovered
+    private Integer AmountOfHealingPower;
+
+    public Apple(){
+        this.biome = biome;
+        this.hasHealingPower = true;
+        this.exchangeable = true;
+        //default constructor added for building inventory
+    }
+
+
+    @Override
+    public String getName() {
+        return "Apple";
+    }
+
+
+    /**
+     * Returns the biome the sand is situated in
+     * @return the biome the sand is situated in
+     */
+    public String getBiome(){
+        return biome;
+    }
+
+    /**
+     * Returns the integer of healing power
+     *
+     * @return the integer of healing power
+     */
+
+    public Integer AmountOfHealingPower(Integer AmountOfRecoverHP){
+        AmountOfHealingPower=AmountOfRecoverHP;
+
+        return AmountOfHealingPower;
+    }
+
+
+
+}
