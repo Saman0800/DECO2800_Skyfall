b4b6345eea0c8e650624baae021a1e1859ea7611
45109762 <zheng0451088698@gmail.com>
2019-08-11 17:33:42 +1000

    Fixed the errors in child class wood

--- src/main/java/deco2800/skyfall/resources/items/Wood.java
+++ src/main/java/deco2800/skyfall/resources/items/Wood.java
@@ -5,34 +5,87 @@ import deco2800.skyfall.resources.NaturalResources;
 import deco2800.skyfall.util.HexVector;
 import deco2800.skyfall.worlds.Tile;
 
-public abstract class Wood extends NaturalResources implements Item {
+public class Wood extends NaturalResources implements Item {
 
-    public Wood(String name, Tile position){
-        super(name, position);
+    // the name of the item
+    private String name;
+
+
+/*    // can the item be stored in the inventory
+    private Boolean carryable;
+
+    // the name of the subtype the item belongs to
+    private String subtype;
+
+    // does the item impact the player's health
+    private Boolean hasHealingPower;
+
+    // the co-ordinates of the tile the item has been placed on
+    private HexVector position;
+
+    // determines whether or not the resource can be traded
+    private Boolean exchangeable;*/
+
+    // the colour of the wood
+    private String colour;
+
+    // the biome the wood is in (will change to different type in future?)
+    private String biome;
+
+    /**
+     * Creates a default wood item
+     */
+    public Wood(){
+        //default constructor added for building inventory
+        this.name = "Wood";
+        this.colour = "brown";
+        this.biome = biome;
+
     }
 
+    /**
+     * Returns the name of the natural resource
+     * @return The name of the natural resource
+     */
     @Override
     public String getName() {
-        return null;
+        return "Wood";
     }
 
-    @Override
-    public Boolean hasHealingPower() {
-        return null;
+
+    /**
+     * Returns the colour of the wood
+     * @return the colour of the wood
+     */
+    public String getColour(){
+        return colour;
     }
 
-    @Override
-    public HexVector getCoords() {
-        return null;
+    /**
+     * Returns the biome the wood is situated in
+     * @return the biome the wood is situated in
+     */
+    public String getBiome(){
+        return biome;
     }
 
+    /**
+     * Creates a string representation of the natural resource in the format:
+     *
+     * <p>'{Nautral Resource}:{Name}'
+     *
+     * <p>without surrounding quotes and with {natural resource} replaced by
+     * the subtype of the item and {name} replaced with the item name
+     * For example:
+     *
+     * <p>Natural Resource:Wood
+     *
+     * @return A string representation of the natural resource.
+     */
     @Override
-    public String getSubtype() {
-        return null;
+    public String toString() {
+        return "" + subtype + ":" + name;
     }
 
-    @Override
-    public Boolean isCarryable() {
-        return null;
-    }
+
 }
