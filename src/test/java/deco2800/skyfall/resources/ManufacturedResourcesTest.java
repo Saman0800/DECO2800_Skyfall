590bbc6dd4ccc180ae1fd40ca5d1b4fa0b371651
45109762 <zheng0451088698@gmail.com>
2019-08-12 22:00:06 +1000

    Added the empty test for Manufactured Resources class, some new features could be used in ManufacturedResources class in the future

---
+++ src/test/java/deco2800/skyfall/resources/ManufacturedResourcesTest.java
@@ -0,0 +1,89 @@
+package deco2800.skyfall.resources;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ManufacturedResourcesTest extends ManufacturedResources {
+
+    private ManufacturedResources manufacturedResource;
+
+    @Before
+    public void setUp() {
+
+    }
+
+    @After
+    public void tearDown() {
+
+    }
+
+    @Test
+    public void getName3() {
+
+    }
+
+    @Test
+    public void isCarryable3() {
+
+    }
+
+
+    @Test
+    public void getSubtype3() {
+
+    }
+
+    @Test
+    public void hasHealingPower3() {
+
+    }
+
+    @Test
+    public void ValueOfStrength() {
+        //Strength of Manufactured Resources
+
+    }
+
+    @Test
+    public void ValueOfEndurance() {
+        //Endurance of Manufactured Resources
+    }
+
+    @Test
+    public void getCoords3() {
+
+    }
+
+
+    @Test
+    public void getCost() {
+        //How much
+
+    }
+
+    @Test
+    public void getSynthesizedResources() {
+                //boolean, if could make up by Natural Resources
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
