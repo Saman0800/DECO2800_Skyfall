f8aa579a613f3e3abf7bb6af626f52915d4a697a
stella_lahanas <s.lahanas@uqconnect.edu.au>
2019-08-13 19:36:06 +1000

    Created tests for the Natural Resources Abstract Class.

--- src/test/java/deco2800/skyfall/resources/NaturalResourcesTest.java
+++ src/test/java/deco2800/skyfall/resources/NaturalResourcesTest.java
@@ -1,6 +1,6 @@
 package deco2800.skyfall.resources;
 
-import deco2800.skyfall.resources.items.Wood;
+import deco2800.skyfall.resources.items.*;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -9,12 +9,19 @@ import static org.junit.Assert.*;
 
 public class NaturalResourcesTest extends NaturalResources {
 
-    private NaturalResources naturalResource;
+    private NaturalResources wood;
+    private NaturalResources sand;
+    private NaturalResources stone;
+    private NaturalResources metal;
+    private NaturalResources vine;
 
     @Before
     public void setUp() throws Exception {
-        naturalResource = new Wood();
-
+        wood = new Wood();
+        sand = new Sand();
+        stone = new Stone();
+        metal = new Metal();
+        vine = new Vine();
     }
 
     @After
@@ -23,17 +30,30 @@ public class NaturalResourcesTest extends NaturalResources {
 
     @Test
     public void getName1() {
-        assertEquals("Wood", naturalResource.getName());
+        assertEquals("Wood", wood.getName());
+        assertEquals("Sand", sand.getName());
+        assertEquals("Stone", stone.getName());
+        assertEquals("Metal", metal.getName());
+        assertEquals("Vine", vine.getName());
     }
 
     @Test
     public void isCarryable1() {
-        assertTrue(naturalResource.isCarryable());
+
+        assertTrue(wood.isCarryable());
+        assertTrue(sand.isCarryable());
+        assertTrue(stone.isCarryable());
+        assertTrue(metal.isCarryable());
+        assertTrue(vine.isCarryable());
     }
 
     @Test
     public void getSubtype1() {
-        assertEquals("Natural Resource", naturalResource.getSubtype());
+        assertEquals("Natural Resource", wood.getSubtype());
+        assertEquals("Natural Resource", sand.getSubtype());
+        assertEquals("Natural Resource", stone.getSubtype());
+        assertEquals("Natural Resource", metal.getSubtype());
+        assertEquals("Natural Resource", vine.getSubtype());
     }
 
 
@@ -43,12 +63,20 @@ public class NaturalResourcesTest extends NaturalResources {
 
     @Test
     public void isExchangeable1() {
-        assertTrue(naturalResource.isExchangeable());
+        assertTrue(wood.isExchangeable());
+        assertTrue(sand.isExchangeable());
+        assertTrue(stone.isExchangeable());
+        assertTrue(metal.isExchangeable());
+        assertTrue(vine.isExchangeable());
     }
 
     @Test
     public void toString1() {
-        assertEquals("Natural Resource:Wood", naturalResource.toString());
+        assertEquals("Natural Resource:Wood", wood.toString());
+        assertEquals("Natural Resource:Sand", sand.toString());
+        assertEquals("Natural Resource:Stone", stone.toString());
+        assertEquals("Natural Resource:Metal", metal.toString());
+        assertEquals("Natural Resource:Vine", vine.toString());
     }
 
 
