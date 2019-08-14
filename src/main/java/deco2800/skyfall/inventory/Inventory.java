ac1e6ef4ba8645138e27398bf481d2b309c38053
Kathryn Bird <k.bird@uq.net.au>
2019-08-14 16:58:08 +1000

    Change data storage within inventory

--- src/main/java/deco2800/skyfall/inventory/Inventory.java
+++ src/main/java/deco2800/skyfall/inventory/Inventory.java
@@ -6,110 +6,91 @@ import deco2800.skyfall.resources.items.Wood;
 import java.util.*;
 import java.lang.*;
 
-/***
- * The inventory associated with the player's current game, and stores collected
- * resources. Able to add and remove items from the inventory, as well as return
- * inventory contents.
- */
+
 public class Inventory {
     //Map that stores the inventory contents
-    private Map<Class<? extends Item>, Integer> inventory;
+    private Map<String, List<Item>> inventory;
 
-    //private List<Item> quickAccessInventory;
+    //Quick Access Inventory
+    private List<Item> quickAccessInventory;
 
 
-    /***
-     *Create an inventory and add initial default items to the inventory.
-     */
+
     public Inventory(){
-        this.inventory = new HashMap<Class<? extends Item>, Integer>();
+        this.inventory = new HashMap<String, List<Item>>();
 
         //Add default items to inventory - eventually find a way to populate more flexibly
         this.inventoryAdd(new Stone());
         this.inventoryAdd(new Stone());
         this.inventoryAdd(new Wood());
         this.inventoryAdd(new Wood());
-
     }
 
-    /***
-     * Create an inventory and populate it with custom mappings of resource
-     * type to the number of that particular resource.
-     * @param inventory Map of Class to integer containing inventory items to add
-     */
+
     public Inventory(Map<Class<? extends Item>, Integer> inventory){
-        this.inventory = new HashMap<Class<? extends Item>, Integer>();
 
-        //Add inventoryContents to inventory
-        this.inventory.putAll(inventory);
 
     }
 
 
-    /***
-     * Return the inventory.
-     * @return Map that represents the inventory.
-     */
-    public Map<Class<? extends Item>, Integer> getInventory(){
-        return Collections.unmodifiableMap(this.inventory);
+    public void getInventory(){
+        System.out.println(this.inventory);
     }
 
 
-    /***
-     * Print the inventory
-     */
+
     public void printInventory(){
-        //Print inventory
+        //Print inventory with numbers
     }
 
 
-    /***
-     * Sort inventory items into sub types for display
-     */
+
     public void displayInventory(){
         //Sort Items into types for display in GUI and return as 3 Maps rather than 1
     }
 
 
-    /***
-     * Add an item to the inventory.
-     * @param item Item to add to the inventory.
-     */
+
     public void inventoryAdd(Item item){
-        if(item.isCarryable()){
-            Class<? extends Item> type = item.getClass();
+        String name = item.getName();
 
-            if(inventory.get(type) != null){
+        if(this.inventory.get(name) != null){
 
-                inventory.put(type, inventory.get(type) + 1);
-
-            }else{
-                inventory.put(type, 1);
-            }
+            List<Item> itemsList = this.inventory.get(name);
+            itemsList.add(item);
+            this.inventory.put(name, itemsList);
 
         }else{
-            //Eventually make this throw an exception?
-            System.out.println("Item can not be carried in the inventory");
-        }
+            List<Item> itemsList = new ArrayList<>();
+            itemsList.add(item);
+            this.inventory.put(name, itemsList);
 
+        }
     }
 
 
-    /***
-     * Remove an item from the inventory.
-     * @param type The type of item to remove
-     */
-    public void inventoryDrop(Class<? extends Item> type){
-        Integer num = this.inventory.get(type);
 
-        if(num == null){
-            System.out.println("You can't remove what you don't have!");
-        } else if(num == 1){
-            this.inventory.remove(type);
-        } else if(num > 1){
-            this.inventory.put(type, inventory.get(type) - 1);
+    public Item inventoryDrop(String itemName){
+
+        if(this.inventory.get(itemName) != null){
+            Integer num = this.inventory.get(itemName).size();
+
+            if(num == 1){
+                Item item = this.inventory.get(itemName).get(0);
+                this.inventory.remove(itemName);
+                return item;
+            } else if(num > 1){
+                List<Item> itemsList = this.inventory.get(itemName);
+                Item item = itemsList.get(num-1);
+                itemsList.remove(num-1);
+                this.inventory.put(itemName, itemsList);
+                return item;
+            }
+
         }
 
+        System.out.println("You can't remove what you don't have!");
+        return null;
     }
 
 
