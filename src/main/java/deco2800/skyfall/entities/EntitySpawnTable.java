3425b19fd5f2467906f5ab48d32bd549316a0161
Chalinda Dinusha Rodrigo <chalindarodrigo@gmail.com>
2019-08-17 19:54:24 +1000

    Supressed a warning on an unchecked method

--- src/main/java/deco2800/skyfall/entities/EntitySpawnTable.java
+++ src/main/java/deco2800/skyfall/entities/EntitySpawnTable.java
@@ -14,6 +14,7 @@ import java.util.Random;
  * Uses EntitySpawnRule for precise spawning
  * however, simpler methods exist that will handle this for you
  */
+@SuppressWarnings("unchecked")
 public class EntitySpawnTable {
     /**
      * Simple static method for placing static items
