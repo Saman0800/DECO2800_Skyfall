9588ed04bd825e48ac32678ba51d870fea3170ca
totaland <james.nguyen868@gmail.com>
2019-08-17 14:15:08 +1000

    add MeleeWeapon, RangeWeapon, Axe, Bow, Spear, Sword and all the test file for these classes

--- src/main/java/deco2800/skyfall/entities/RangeWeapon.java
+++ src/main/java/deco2800/skyfall/entities/RangeWeapon.java
@@ -1,10 +1,23 @@
 package deco2800.skyfall.entities;
 
-public class RangeWeapon extends Weapon {
+public abstract class RangeWeapon extends AbstractEntity implements IWeapon, Item {
 
-    public RangeWeapon() {
-
+    /**
+     * what is the type of the weapon
+     * @return a string "melee" represent all melee weapon
+     */
+    @Override
+    public String getWeaponType() {
+        return "range";
     }
 
+    /**
+     * What is the damage type of a weapon
+     * @return a string "splash" for bow and spear
+     */
+    @Override
+    public String getDamageType() {
+        return "splash";
+    }
 
 }
