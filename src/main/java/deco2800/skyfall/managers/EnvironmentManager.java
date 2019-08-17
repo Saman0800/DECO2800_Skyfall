112eab237f93445ba6c46c0720c8dce521c9da65
kelsey_quinn <s4172433@student.uq.edu.au>
2019-08-17 16:22:12 +1000

    Updates to EnvironmentManager

--- src/main/java/deco2800/skyfall/managers/EnvironmentManager.java
+++ src/main/java/deco2800/skyfall/managers/EnvironmentManager.java
@@ -3,32 +3,60 @@ package deco2800.skyfall.managers;
 import deco2800.skyfall.worlds.*;
 
 public class EnvironmentManager {
+   //Game Time variable
+   private long gameTime;
+
+   //Hours in a game day
+   private long hours;
+
+   //Night hours in a game day
+   private long pmHours;
+
+   //Biome the player is in
+   private AbstractBiome biome;
 
    /**
     * Constructor
-    * Returns time of day
+    *
     */
    public EnvironmentManager(long i) {
-      long gameTime = i;
+      gameTime = i;
 
       //Each day cycle goes for approx 24 minutes
-      long hours = (gameTime/ 60000);
-      hours %= 24;
+      long time = (gameTime/ 60000);
+      hours = time % 24;
 
-      long pmHours = hours;
+      pmHours = hours;
 
       //Refactor hours to standard time
       if (hours > 12 && hours < 24) {
          pmHours = hours - 12;
       }
-
-      //Print time
-      if (hours > 11 && hours < 24) {
-         //System.out.println(pmHours + "pm");
-      } else {
-         //System.out.println(hours + "am");
-      }
    }
+
+   /**
+    * Gets time of day in game
+    *
+    * @return long The time of day
+    */
+   public long getTime() {
+      if (hours > 11 && hours < 24) {
+         return pmHours;
+         //System.out.println(pmHours + "pm");
+      }
+      //System.out.println(hours + "am");
+      return hours;
+   }
+
+   /**
+    * Sets the time of day in game
+    *
+    * @param time The time of day to be set
+    */
+   public void setTime(int time) {
+      hours = time;
+   }
+
 
    public void setBiomeMusic() {
       //get biome and play music accordingly
