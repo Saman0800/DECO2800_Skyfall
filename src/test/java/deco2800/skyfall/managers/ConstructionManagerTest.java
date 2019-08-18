69999b388094181a198c80863a0b548833ad33c1
Steven_real <1046318912@qq.com>
2019-08-19 04:33:16 +0800

    add unit tests for inventory methods

--- src/test/java/deco2800/skyfall/managers/ConstructionManagerTest.java
+++ src/test/java/deco2800/skyfall/managers/ConstructionManagerTest.java
@@ -1,5 +1,7 @@
 package deco2800.skyfall.managers;
 
+import deco2800.skyfall.entities.structures.AbstractBuilding;
+import deco2800.skyfall.entities.structures.WallBuilding;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
@@ -9,6 +11,8 @@ import deco2800.skyfall.managers.ConstructionManager;
 import deco2800.skyfall.worlds.AbstractWorld;
 import deco2800.skyfall.worlds.Tile;
 import deco2800.skyfall.entities.AbstractEntity;
+
+import java.util.TreeMap;
 
 //Add all tests related to the construction manager
 public class ConstructionManagerTest {
@@ -20,16 +24,54 @@ public class ConstructionManagerTest {
     }
 
     @Test
+    public void testInvCheckPositive(){
+        AbstractBuilding building = new WallBuilding(1,1);
+        TreeMap<String, Integer> buildingCost = new TreeMap<>();
+        buildingCost.put("Stone", 2);
+        buildingCost.put("Wood", 2);
+        building.setCost(buildingCost);
+
+        InventoryManager inventoryManager = new InventoryManager();
+        Boolean result = cmgr.invCheck(building, inventoryManager);
+
+        Assert.assertTrue(result);
+    }
+
+    @Test
+    public void testInvCheckNegative(){
+        AbstractBuilding building = new WallBuilding(1,1);
+        TreeMap<String, Integer> buildingCost = new TreeMap<>();
+        buildingCost.put("Stone", 3);
+        buildingCost.put("Wood", 2);
+        building.setCost(buildingCost);
+
+        InventoryManager inventoryManager = new InventoryManager();
+        Boolean result = cmgr.invCheck(building, inventoryManager);
+
+        Assert.assertFalse(result);
+    }
+
+    @Test
+    public void testInvRemove(){
+        AbstractBuilding building = new WallBuilding(1,1);
+        TreeMap<String, Integer> buildingCost = new TreeMap<>();
+        buildingCost.put("Stone", 2);
+        buildingCost.put("Wood", 2);
+        building.setCost(buildingCost);
+
+        InventoryManager inventoryManager = new InventoryManager();
+        cmgr.invRemove(building, inventoryManager);
+
+        Assert.assertEquals(0, inventoryManager.getAmount("Stone"));
+        Assert.assertEquals(0, inventoryManager.getAmount("Wood"));
+    }
+
+    @Test
     public void verifyNullTest() {
         Assert.assertEquals(false, this.cmgr.verifyTerrain(null));
         Assert.assertEquals(false, this.cmgr.verifyBiome(null));
         Assert.assertEquals(false, this.cmgr.verifyEntity(null, null));
         Assert.assertEquals(false, this.cmgr.isTilesBuildable(null, null));
-    }
-
-    @Test
-    public void testVerifyEntity(){
-        Assert.assertFalse(cmgr.verifyEntity(null, null));
     }
 
     @After
