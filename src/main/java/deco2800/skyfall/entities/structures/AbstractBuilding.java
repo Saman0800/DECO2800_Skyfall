b4eed4ad77828acde354c490cb4bccf971221902
Byron Maxwell <b.maxwell@uqconnect.edu.au>
2019-08-19 09:37:12 +1000

    Place and Remove methods

--- src/main/java/deco2800/skyfall/entities/structures/AbstractBuilding.java
+++ src/main/java/deco2800/skyfall/entities/structures/AbstractBuilding.java
@@ -2,6 +2,7 @@ package deco2800.skyfall.entities.structures;
 
 //Needs to extend this to render buildings
 import deco2800.skyfall.entities.AbstractEntity;
+import deco2800.skyfall.worlds.AbstractWorld;
 
 /**
  * An abstract building is an item that can be placed in the world
@@ -42,12 +43,25 @@ public abstract class AbstractBuilding extends AbstractEntity {
      */
     public void setYcoord(int newYcoord) {this.ycoord = newYcoord;}
 
-    //place method
-    public void placeBuilding(float x, float y, int height) {
+    /**
+     *
+     * @param x - X coordinate
+     * @param y - Y coordinate
+     * @param height - Render height
+     * @param world - World to place building in
+     */
+    public void placeBuilding(float x, float y, int height, AbstractWorld world) {
         setPosition(x, y, height);
+        world.addEntity(this);
     }
 
-    //remove method
+    /**
+     *
+     * @param world - World to remove building from
+     */
+    public void removeBuilding(AbstractWorld world) {
+        world.removeEntity(this);
+    }
 
     //Interaction method
 
