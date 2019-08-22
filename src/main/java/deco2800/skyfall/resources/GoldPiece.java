4fc0ca103ee9721f7b47402c1451661495d2f4ad
stella_lahanas <s.lahanas@uqconnect.edu.au>
2019-08-22 13:47:43 +1000

    Added comments to the gold piece class.

--- src/main/java/deco2800/skyfall/resources/GoldPiece.java
+++ src/main/java/deco2800/skyfall/resources/GoldPiece.java
@@ -1,12 +1,24 @@
 package deco2800.skyfall.resources;
 
+/**
+ * The civilisation's currency is Gold Pieces valued as either 5G,
+ * 10G, 50G or 100G
+ */
 public class GoldPiece {
+
+    // the value of the piece of gold (either 5G,10G,50G or 100G)
     public int value;
 
+    /**
+     * Creates a gold piece with a particular value.
+     * @param value The value of the piece of gold, either 5G, 10G, 50G or 100G.
+     */
     public GoldPiece(int value){
         if (value == 5 || value == 10 || value == 50 || value == 100){
             this.value = value;
-        } System.out.println("This is not a valid gold piece");
+        } else {
+            System.out.println("Invalid piece of gold");
+        }
 
     }
 }
