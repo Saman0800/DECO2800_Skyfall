eea6b4bf5f84965349a944962a3ba7b40a6b0cea
stella_lahanas <s.lahanas@uqconnect.edu.au>
2019-08-25 07:52:18 +1000

    Created a GoldPieceTest class.

---
+++ src/test/java/deco2800/skyfall/resources/GoldPieceTest.java
@@ -0,0 +1,22 @@
+package deco2800.skyfall.resources;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class GoldPieceTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void getValue() {
+    }
+}
