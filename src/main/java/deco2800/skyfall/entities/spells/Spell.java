d5713863db71e6a290a93ca79ff76229d5c88263
Christopher Poli <chris.luke.poli@gmail.com>
2019-09-03 23:19:19 +1000

    Added the Spell class.

---
+++ src/main/java/deco2800/skyfall/entities/spells/Spell.java
@@ -0,0 +1,25 @@
+package deco2800.skyfall.entities.spells;
+
+import deco2800.skyfall.entities.Projectile;
+import deco2800.skyfall.util.HexVector;
+
+public class Spell extends Projectile {
+
+    /**
+     * Construct a new spell.
+     *
+     * @param movementPosition
+     * @param textureName      The name of the texture to render.
+     * @param objectName       The name to call this object.
+     * @param col              The column to spawn this projectile in.
+     * @param row              The row to spawn this projectile in.
+     * @param damage           The damage this projectile will deal on hit.
+     * @param speed            How fast this projectile is travelling.
+     * @param range
+     */
+    public Spell(HexVector movementPosition, String textureName, String objectName, float col, float row, int damage, float speed, int range) {
+        super(movementPosition, textureName, objectName, col, row, damage, speed, range);
+
+
+    }
+}
