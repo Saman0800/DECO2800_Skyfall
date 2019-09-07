4126bb2ab1f648bde5a51c046c17f080e239203f
Christopher Poli <chris.luke.poli@gmail.com>
2019-09-07 15:24:39 +1000

    Add commenting to StatBar.

--- src/main/java/deco2800/skyfall/gui/StatBar.java
+++ src/main/java/deco2800/skyfall/gui/StatBar.java
@@ -4,6 +4,9 @@ import com.badlogic.gdx.scenes.scene2d.Stage;
 
 /**
  * We have a HealthCircle and a Manabar, need to join some common logic.
+ *
+ * Manabar and healthcircle will perform almost identical with different textures.
+ * Just need to pull the mana instead of health from mc.
  */
 public class StatBar {
 
