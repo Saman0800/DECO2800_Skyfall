3531117b06451d094fc49f41432bde4d073b3b1e
Luobin_real <1046318912@qq.com>
2019-09-09 23:57:36 +1000

    add invetoryManager into Building to manage the storage

--- src/main/java/deco2800/skyfall/entities/structures/AbstractBuilding.java
+++ src/main/java/deco2800/skyfall/entities/structures/AbstractBuilding.java
@@ -1,6 +1,8 @@
 package deco2800.skyfall.entities.structures;
 
 import deco2800.skyfall.entities.AbstractEntity;
+import deco2800.skyfall.resources.Item;
+import deco2800.skyfall.managers.InventoryManager;
 import deco2800.skyfall.worlds.world.World;
 
 import java.util.Map;
@@ -21,6 +23,10 @@ public abstract class AbstractBuilding extends AbstractEntity {
     private int buildTime;
 
     private Map<String, Integer> buildCost = new TreeMap<>();
+
+    private InventoryManager inventoryManager;
+
+//    private Boolean hasPlayer;
 
     /**
      *
@@ -123,4 +129,31 @@ public abstract class AbstractBuilding extends AbstractEntity {
      */
     public int getBuildTime() {return this.buildTime;}
 
+    /**
+     * getter method
+     * @return InventoryManager of the Building
+     */
+    public InventoryManager getInventoryManager() {
+        return this.inventoryManager;
+    }
+
+    /**
+     *
+     * @param inventoryManager
+     */
+    public void setInventoryManager(InventoryManager inventoryManager) {
+        this.inventoryManager = inventoryManager;
+    }
+
+    public boolean AddInvetory(Item item){
+        return this.inventoryManager.inventoryAdd(item);
+    }
+
+    /**
+     *
+     * @param item
+     */
+    public void quickAccessRemove(Item item){
+        this.inventoryManager.quickAccessRemove(item.getName());
+    }
 }
