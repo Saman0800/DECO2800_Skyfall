4253607c33e02e8ff5e5d3db9b3bc03ccc56000a
Christopher Poli <chris.luke.poli@gmail.com>
2019-09-10 12:29:04 +1000

    Tornado now works as intended.

--- src/main/java/deco2800/skyfall/entities/spells/Tornado.java
+++ src/main/java/deco2800/skyfall/entities/spells/Tornado.java
@@ -1,13 +1,22 @@
 package deco2800.skyfall.entities.spells;
 
+import deco2800.skyfall.entities.MainCharacter;
+import deco2800.skyfall.managers.GameManager;
+import deco2800.skyfall.managers.GameMenuManager;
 import deco2800.skyfall.util.HexVector;
 
 public class Tornado extends Spell {
+
+    private MainCharacter mc;
 
     public Tornado(HexVector movementPosition, String textureName, String objectName,
                   float col, float row, int damage, float speed, int range) {
 
         super(movementPosition, textureName, objectName, col, row, damage, speed, range);
+
+        this.mc = GameManager.getManagerFromInstance(GameMenuManager.class).getMainCharacter();
+
+        this.position = new HexVector(this.mc.getCol(), this.mc.getRow());
 
         this.manaCost = 10;
 
--- src/test/java/deco2800/skyfall/gui/ManaBarTest.java
+++ src/test/java/deco2800/skyfall/gui/ManaBarTest.java
@@ -1,6 +1,9 @@
 package deco2800.skyfall.gui;
 
-public class ManaBarTest {
+import deco2800.skyfall.BaseGDXTest;
+
+public class ManaBarTest extends BaseGDXTest {
+
 
 
 }
