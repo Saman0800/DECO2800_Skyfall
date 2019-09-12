461d59c744d6d5a1eb30af0c1905ee369cb5ed8e
Chalinda Dinusha Rodrigo <chalindarodrigo@gmail.com>
2019-09-12 21:10:48 +1000

    Added float testing for clamp

--- src/test/java/deco2800/skyfall/util/MathUtilTest.java
+++ src/test/java/deco2800/skyfall/util/MathUtilTest.java
@@ -7,7 +7,7 @@ import static org.junit.Assert.assertEquals;
 
 public class MathUtilTest {
     @Test
-    public void testClamp() {
+    public void testClampInt() {
         //int test
         int value = clamp(-10, 0, 10);
         assertEquals(0, value);
@@ -15,5 +15,16 @@ public class MathUtilTest {
         assertEquals(5, value);
         value = clamp(20, 0, 10);
         assertEquals(10, value);
+    }
+
+    @Test
+    public void testClampFloat() {
+        //float test
+        float value = clamp(-10.0f, 0.0f, 10.0f);
+        assertEquals(0.0f, value, 0.1f);
+        value = clamp(5.0f, 0.0f, 10.0f);
+        assertEquals(5.0f, value, 0.1f);
+        value = clamp(20.0f, 0.0f, 10.0f);
+        assertEquals(10.0f, value, 0.1f);
     }
 }
