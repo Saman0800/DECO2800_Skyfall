ad82a82ec46b73209ec84b73aebccef9a577192e
Peng Shu <s4518373@student.uq.edu.au>
2019-09-15 22:05:48 +1000

    change location of whitebear

--- src/main/java/deco2800/skyfall/entities/pets/IceWhitebear.java
+++ src/main/java/deco2800/skyfall/entities/pets/IceWhitebear.java
@@ -48,7 +48,7 @@ public class IceWhitebear extends AbstractPet implements Animatable, Harvestable
     @Override
     public List<AbstractEntity> harvest(Tile tile) {
         List<AbstractEntity> abstractEntityList=new ArrayList<>();
-        Whitebear whitebear=new Whitebear(0, 4,mc);
+        Whitebear whitebear=new Whitebear(-2, 0,mc);
         whitebear.setDomesticated(false);
         abstractEntityList.add(whitebear);
         return abstractEntityList;
