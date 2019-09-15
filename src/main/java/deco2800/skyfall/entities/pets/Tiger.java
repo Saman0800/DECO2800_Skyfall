ae4f8e17d0012c0fab331c6447703aae1c488468
45109762 <zheng0451088698@gmail.com>
2019-09-15 22:21:06 +1000

    Put the tiger class into pet package

--- src/main/java/deco2800/skyfall/entities/pets/Tiger.java
+++ src/main/java/deco2800/skyfall/entities/pets/Tiger.java
@@ -1,4 +1,4 @@
-package deco2800.skyfall.entities;
+package deco2800.skyfall.entities.pets;
 
 import deco2800.skyfall.animation.Animatable;
 import deco2800.skyfall.animation.AnimationLinker;
@@ -195,9 +195,9 @@ public class Tiger extends EnemyEntity implements Animatable {
     public void runAway(){
         targetPosition = new float[2];
         targetPosition[0] = (float)
-                (Math.random() * 700 + orginalPosition[0]);
+                (Math.random() * 800 + orginalPosition[0]);
         targetPosition[1]=(float)
-                (Math.random() * 700 + orginalPosition[1]);
+                (Math.random() * 800 + orginalPosition[1]);
         float[] randomPositionWorld = WorldUtil.worldCoordinatesToColRow
                 (targetPosition[0], targetPosition[1]);
         destination = new HexVector(randomPositionWorld[0],
@@ -221,11 +221,11 @@ public class Tiger extends EnemyEntity implements Animatable {
      *
      */
     private void randomMoving() {
-        if(moving==false){
+        if(moving == false){
             targetPosition = new float[2];
             targetPosition[0] = (float)
                     (Math.random() * 200 + orginalPosition[0]);
-            targetPosition[1]=(float)
+            targetPosition[1] = (float)
                     (Math.random() * 200 + orginalPosition[1]);
             float[] randomPositionWorld = WorldUtil.worldCoordinatesToColRow
                     (targetPosition[0], targetPosition[1]);
@@ -260,7 +260,7 @@ public class Tiger extends EnemyEntity implements Animatable {
      */
     int time=0;
     private void tigerDead(){
-        if(time<=100){
+        if(time <= 100){
             time++;
             this.setTexture("tigerDead");
             this.setObjectName("tigerDead");
