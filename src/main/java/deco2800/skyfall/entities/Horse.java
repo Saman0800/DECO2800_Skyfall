35602686e6a02dbd5557a40d29f6386f166c7d27
Gloria CHAN <ch4gloria@gmail.com>
2019-09-16 00:54:45 +1000

    Create a new Horse class

---
+++ src/main/java/deco2800/skyfall/entities/Horse.java
@@ -0,0 +1,71 @@
+package deco2800.skyfall.entities;
+
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import deco2800.skyfall.animation.AnimationRole;
+import deco2800.skyfall.managers.TextureManager;
+import deco2800.skyfall.worlds.biomes.AbstractBiome;
+import deco2800.skyfall.worlds.biomes.DesertBiome;
+
+public class Horse extends VehicleEntity {
+    private AbstractBiome biomeType = new DesertBiome();
+    private static TextureManager textureManager;
+    private Stage stage;
+    private static final transient String BIOME = "desert";
+    private MainCharacter mc;
+    private boolean available = true;
+    private boolean moving=false;
+    private static final transient String VEHICLE = "horse";
+    private static final transient int HEALTH = 10;
+
+    public Horse(float col, float row, MainCharacter mc) {
+        super(col,row);
+        this.mc = mc;
+        this.setTexture("horse");
+        this.setObjectName("horse");
+        this.setHeight(1);
+        this.setAvailable(available);
+        this.setHealth(HEALTH);
+    }
+
+
+    public void setBiome(AbstractBiome biomeType){
+        this.biomeType = biomeType;
+    }
+
+    public String getBiome() {
+        return BIOME;
+    }
+
+    @Override
+    public void onTick(long i) {
+        super.onTick(i);
+        if (mc != null) {
+            float colDistance = mc.getCol() - this.getCol();
+            float rowDistance = mc.getRow() - this.getRow();
+
+            if ((colDistance * colDistance + rowDistance * rowDistance) < 4){
+
+                //TODO: Let main character get onto vehicle
+                setTexture("horse_character");
+                setObjectName("horse_character");
+
+
+            } else {
+                this.setCurrentState(AnimationRole.NULL);
+            }
+        } else {
+            System.out.println("Main Character is null");
+        }
+
+    }
+
+    public String getVehicleType() {
+        return VEHICLE;
+    }
+
+    public boolean getMove() {
+        return this.moving;
+    }
+
+
+}
