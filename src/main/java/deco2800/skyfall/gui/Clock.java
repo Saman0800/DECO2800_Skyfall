2ee61f28d9f0f0c82a7429579e9a5fae23bcee24
s4479623 <ashleighrae.oneill@uqconnect.edu.au>
2019-09-15 17:01:35 +1000

    Updated clock in Clock.java to display current season, updated javadoc.

--- src/main/java/deco2800/skyfall/gui/Clock.java
+++ src/main/java/deco2800/skyfall/gui/Clock.java
@@ -6,49 +6,75 @@ import com.badlogic.gdx.scenes.scene2d.ui.Image;
 import deco2800.skyfall.managers.*;
 
 public class Clock {
+    // Clock image
     private Image clockDisplay;
-    private Stage stage;
+
+    // Clock filename
     private String clockTexture;
 
+    // Stage for images to live
+    private Stage stage;
+
+    // Season image
+    private Image seasonDisplay;
+
+    // Season filename
+    private String seasonTexture;
 
     /**
-     * Constructor
+     * Constructor to create a clock image in game that changes in accordance with time
      * @param s Stage to display things on
      */
     public Clock(Stage s) {
+        // Set stage
         stage = s;
 
-        BitmapFont bitmapFont  = new BitmapFont();
-        bitmapFont.getData().setScale(1f);
-
+        // Set default clock texture
         clockTexture = "dawn";
 
-        this.clockDisplay = new Image(GameMenuManager.generateTextureRegionDrawableObject(clockTexture));
+        // Set default season texture
+        seasonTexture = "summer";
 
+        // Set displays
+        this.clockDisplay = new Image(GameMenuManager.generateTextureRegionDrawableObject(clockTexture));
+        this.seasonDisplay = new Image(GameMenuManager.generateTextureRegionDrawableObject(seasonTexture));
+
+        // Update screen
         updateWithViewportChanges();
 
+        // Add displays onto stage
         stage.addActor(clockDisplay);
+        stage.addActor(seasonDisplay);
     }
 
     /**
-     * Maintains position of clockDisplay with resizes
+     * Maintains position of clockDisplay and seasonDisplay with resizes
      */
     private void updateWithViewportChanges() {
         float positionX;
         float positionY;
 
-        positionX = (stage.getCamera().position.x  + (stage.getCamera().viewportWidth / 2) - 280);
+        positionX = (stage.getCamera().position.x  + (stage.getCamera().viewportWidth / 2) - 300);
         positionY = (stage.getCamera().position.y  +  (stage.getCamera().viewportHeight / 2) - 70);
 
+        // Set clock position
         clockDisplay.setPosition(positionX, positionY);
+
+        // Set season position
+        seasonDisplay.setPosition(positionX + 160, positionY + 10);
     }
 
     /**
      * Updates clockDisplay arrow in accordance with time
      */
     private void updateDisplay() {
+        // Time of day in hours
         int time = GameManager.get().getManager(EnvironmentManager.class).getTime();
 
+        // Current season
+        String season = GameManager.get().getManager(EnvironmentManager.class).getSeason();
+
+        // Monitor hours
         if (GameManager.get().getManager(EnvironmentManager.class).getTOD() != null) {
             if (time >= 5 && time <= 9) {
                 clockTexture = "dawn";
@@ -61,18 +87,24 @@ public class Clock {
             }
             clockDisplay.setDrawable(GameMenuManager.generateTextureRegionDrawableObject(clockTexture));
         }
-    }
 
-    public String getClockTexture() {
-        return clockTexture;
-    }
-
-    public Image getClockDisplay() {
-        return clockDisplay;
+        // Monitor seasons
+        if (season != null) {
+            if (season == "Summer") {
+                seasonTexture = "summer";
+            } else if (season == "Winter") {
+                seasonTexture = "winter";
+            } else if (season == "Autumn") {
+                seasonTexture = "autumn";
+            } else {
+                seasonTexture = "spring";
+            }
+            seasonDisplay.setDrawable(GameMenuManager.generateTextureRegionDrawableObject(seasonTexture));
+        }
     }
 
     /**
-     * Updates the clockDisplay and resizes if necessary
+     * Updates the display and resizes if necessary
      */
     public void update() {
         updateWithViewportChanges();
