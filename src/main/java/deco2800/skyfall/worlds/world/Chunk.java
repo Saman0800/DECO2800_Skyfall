d3e676df4b4d626a8b2d078341ce29864b9d12ce
Ontonator <ontonator@me.com>
2019-09-16 02:20:11 +1000

    Added documentation to `Chunk`.

--- src/main/java/deco2800/skyfall/worlds/world/Chunk.java
+++ src/main/java/deco2800/skyfall/worlds/world/Chunk.java
@@ -28,6 +28,16 @@ public class Chunk implements Saveable<Chunk.ChunkMemento> {
     private ArrayList<Tile> tiles;
     private ArrayList<AbstractEntity> entities;
 
+    /**
+     * Loads the chunk at the specified coordinates and adds it to the world. If the chunk has not been generated yet,
+     * this also generates it.
+     *
+     * @param world the world to which the chunk is added
+     * @param x     the x coordinate of the chunk (in chunk coordinates)
+     * @param y     the y coordinate of the chunk (in chunk coordinates)
+     *
+     * @return the loaded chunk
+     */
     public static Chunk loadChunkAt(World world, int x, int y) {
         // FIXME:Ontonator Load the chunk.
         Chunk chunk = new Chunk(world, x, y);
@@ -35,6 +45,14 @@ public class Chunk implements Saveable<Chunk.ChunkMemento> {
         return chunk;
     }
 
+    /**
+     * Gets the chunk coordinates from world coordinates.
+     *
+     * @param col the world column of the point to check
+     * @param row the world row of the point to check
+     *
+     * @return the coordinates of the corresponding chunk
+     */
     public static Pair<Integer, Integer> getChunkForCoordinates(double col, double row) {
         double tileCol = Math.round(col);
         double tileRowOffset = tileCol % 2 == 0 ? 0 : 0.5;
@@ -47,12 +65,12 @@ public class Chunk implements Saveable<Chunk.ChunkMemento> {
     }
 
     /**
-     * Generate a new chunk from a save
+     * Constructs a new chunk from a save.
      *
-     * @param memento the memento to load from
-     * @param world the world this is in
+     * @param world   the world to which the chunk is added
+     * @param memento the memento from which to load the chunk
      */
-    public Chunk(ChunkMemento memento, World world) {
+    public Chunk(World world, ChunkMemento memento) {
         this(world, memento.x, memento.y);
         this.load(memento);
     }
@@ -65,6 +83,16 @@ public class Chunk implements Saveable<Chunk.ChunkMemento> {
         return world;
     }
 
+    /**
+     * Constructs a new chunk in the specified world at the specified coordinates and backed by the specified {@link
+     * ArrayList}s (i.e. they are used internally).
+     *
+     * @param world    the world to which the chunk is added
+     * @param x        the x coordinate of the chunk
+     * @param y        the y coordinate of the chunk
+     * @param tiles    the list used internally to store the tiles
+     * @param entities the list used internally to store the entities
+     */
     public Chunk(World world, int x, int y, ArrayList<Tile> tiles, ArrayList<AbstractEntity> entities) {
         this.world = world;
         this.x = x;
@@ -76,13 +104,20 @@ public class Chunk implements Saveable<Chunk.ChunkMemento> {
         world.getLoadedChunks().put(new Pair<>(x, y), this);
     }
 
+    /**
+     * Constructs a new cunk in the specified world at the specified coordinates.
+     *
+     * @param world the world to which the chunk is added
+     * @param x     the x coordinate of the chunk
+     * @param y     the y coordinate of the chunk
+     */
     public Chunk(World world, int x, int y) {
         this(world, x, y, new ArrayList<>(), new ArrayList<>());
         generateTiles();
     }
 
     /**
-     * Creates blank tiles which fill the area of this chunk.
+     * Generates the tiles to fill this chunk.
      */
     private void generateTiles() {
         for (int row = y * CHUNK_SIDE_LENGTH; row < (y + 1) * CHUNK_SIDE_LENGTH; row++) {
@@ -100,6 +135,9 @@ public class Chunk implements Saveable<Chunk.ChunkMemento> {
         generateNeighbours();
     }
 
+    /**
+     * Generates the neighbours for the tiles.
+     */
     private void generateNeighbours() {
         // Multiply coords by 2 to remove floats.
         Map<Integer, Map<Integer, Tile>> tileMap = new HashMap<>();
@@ -178,6 +216,9 @@ public class Chunk implements Saveable<Chunk.ChunkMemento> {
         }
     }
 
+    /**
+     * Generates the static entities within this chunk.
+     */
     public void generateEntities() {
         for (Tile tile : getTiles()) {
             for (EntitySpawnRule rule : world.getSpawnRules().get(tile.getBiome())) {
@@ -186,6 +227,9 @@ public class Chunk implements Saveable<Chunk.ChunkMemento> {
         }
     }
 
+    /**
+     * Saves this chunk and removes it from the world.
+     */
     public void unload() {
         for (Tile tile : tiles) {
             tile.removeReferanceFromNeighbours();
@@ -196,14 +240,33 @@ public class Chunk implements Saveable<Chunk.ChunkMemento> {
         // FIXME:Ontonator Save the chunk.
     }
 
+    /**
+     * Gets the tiles in this chunk. This is the list used internally, so modifing it will modify the chunk. <em>Do NOT
+     * modify this list without fully understanding how the chunks work.</em> If you want to modify this list but do not
+     * want to modify the chunk, make a copy first.
+     *
+     * @return the list of tiles
+     */
     public List<Tile> getTiles() {
         return tiles;
     }
 
+    /**
+     * Gets the entities in this chunk. This is the list used internally, so modifing it will modify the chunk. <em>Do
+     * NOT modify this list without fully understanding how the chunks work.</em> If you want to modify this list but do
+     * not want to modify the chunk, make a copy first.
+     *
+     * @return the list of entities
+     */
     public List<AbstractEntity> getEntities() {
         return entities;
     }
 
+    /**
+     * Adds an entity to this chunk. <em>Ensure that the entity belongs in this chunk before adding it.</em>
+     *
+     * @param entity the entity to add to this chunk
+     */
     public void addEntity(AbstractEntity entity) {
         // Binary searching for the correct index and inserting is O(n), compared to O(nlog(n)) for inserting at the end
         // then sorting the list (or possibly O(n²), depending on the implementation of the sort, considering that the
@@ -224,23 +287,48 @@ public class Chunk implements Saveable<Chunk.ChunkMemento> {
         entities.add(entity);
     }
 
+    /**
+     * Removes the entity from this chunk if is is present.
+     *
+     * @param entity the entity to remove.
+     */
     public void removeEntity(AbstractEntity entity) {
         entities.remove(entity);
     }
 
+    /**
+     * Gets the x coordinate of this chunk in chunk coordinates.
+     *
+     * @return the x coordinate of this chunk
+     */
     public int getX() {
         return x;
     }
 
+    /**
+     * Gets the y coordinate of this chunk in chunk coordinates.
+     *
+     * @return the y coordinate of this chunk
+     */
     public int getY() {
         return y;
     }
 
+    /**
+     * Constructs a memento containing the information required to reproduce this chunk.
+     *
+     * @return the memento containing the information require to reproduce this chunk
+     */
     @Override
     public ChunkMemento save() {
         return new ChunkMemento(this);
     }
 
+    /**
+     * Loads the data from the given memento into this chunk.
+     *
+     * @param memento the memento from which to load the information
+     */
     @Override
     public void load(ChunkMemento memento) {
         this.x = memento.x;
@@ -248,12 +336,20 @@ public class Chunk implements Saveable<Chunk.ChunkMemento> {
         this.tiles = memento.tiles;
     }
 
-    public class ChunkMemento extends AbstractMemento {
+    /**
+     * The memento storing the information required to reproduce a chunk.
+     */
+    public static class ChunkMemento extends AbstractMemento {
         private int x;
         private int y;
         private ArrayList<Tile> tiles;
         private ArrayList<Integer> entities;
 
+        /**
+         * Constructs a memento with the information required to reproduce the given chunk.
+         *
+         * @param chunk the chunk to be saved
+         */
         public ChunkMemento(Chunk chunk) {
             this.x = chunk.x;
             this.y = chunk.y;
