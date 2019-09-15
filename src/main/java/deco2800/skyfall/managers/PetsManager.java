4b3a335a5105050a3063290b6baef5e87d116093
haoranpan <haoranpan3612089@gmail.com>
2019-09-16 00:27:33 +1000

    pets manager

--- src/main/java/deco2800/skyfall/managers/PetsManager.java
+++ src/main/java/deco2800/skyfall/managers/PetsManager.java
@@ -4,15 +4,24 @@ import deco2800.skyfall.Tickable;
 import deco2800.skyfall.entities.MainCharacter;
 import deco2800.skyfall.entities.pets.AbstractPet;
 import deco2800.skyfall.resources.Item;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.*;
 
 public class PetsManager extends AbstractManager {
-    private static final int MAX_PETS_Summon = 1;
+    //use map to store Pets Key is the name of pet
+    //coder can use pet name to get pet
     private Map<String, AbstractPet> petMap;
+
+    //Store pets in a list
     private LinkedList<AbstractPet> petList;
+
+    //put pets in the character inventory
     private InventoryManager inventoryManager;
 
+    //logger
+    private static final Logger LOGGER = LoggerFactory.getLogger(SoundManager.class);
 
     public PetsManager() {
         this.petMap = new HashMap<>();
@@ -20,19 +29,27 @@ public class PetsManager extends AbstractManager {
         inventoryManager=GameManager.getManagerFromInstance(InventoryManager.class);
     }
 
-
-    public  Collection<AbstractPet> allPets(){
-        for(List<Item> itemList:this.inventoryManager.getInventoryContents().values()){
-            for(Item item:itemList){
-                if(item instanceof AbstractPet){
-                    petMap.put(item.getName(), (AbstractPet) item);
-                    petList.add((AbstractPet) item);
-                }
-            }
-        }
-        return petMap.values();
+    /**
+     * add a pet into pets map
+     * @param ap
+     */
+    public void addPet(AbstractPet ap){
+        petMap.put(ap.getName(),ap);
+        petList.add(ap);
     }
 
+    /**
+     * Get all of pets in the inventory
+     * @return all of pets
+     */
+    public  LinkedList<AbstractPet> allPets(){
+        return petList;
+    }
+
+    /**
+     * The pet which is summoned right now
+     * @return the summoned pet
+     */
     public AbstractPet currentSummonedPet(){
         for(AbstractPet ap:petMap.values()){
             if(ap.isSummoned()){
@@ -42,47 +59,72 @@ public class PetsManager extends AbstractManager {
         return null;
     }
 
+    //index used to switch to next pet
+    private int i=-1;
+
+    /**
+     * Summon another pet and put the original pet into inventory
+     * @param mc main character
+     */
     public void replacePet(MainCharacter mc){
-        System.out.println(currentSummonedPet());
+        i++;
         if(petList.size()>0){
+            //if no pet was summoned then call the first pet
             if(currentSummonedPet()==null){
                 this.inventoryManager.quickAccessRemove(petList.get(0).getName());
                 AbstractPet pet=petList.get(0);
+                pet.setCol(mc.getCol()-1);
+                pet.setRow(mc.getRow()-1);
                 pet.setSummoned(true);
                 pet.setDomesticated(true);
                 GameManager.get().getWorld().addEntity(pet);
             }else{
-                AbstractPet abstractPet=currentSummonedPet();
-                int i=petPositionInList(abstractPet);
-                if(i+1<petList.size()){
-                    summonNext(i);
+                //switch to the next pet
+                if(i<petList.size()){
+                    summonNext(i,mc);
                 }else{
                     i=0;
-                    summonNext(i);
+                    summonNext(i,mc);
+
                 }
             }
         }else{
-            System.out.println("no pet available");
+            LOGGER.info("no pet available right now");
         }
 
 
     }
 
 
-
-    private void summonNext(int i){
-        AbstractPet pet=petList.get(i+1);
-        petList.get(i).setSummoned(false);
-        this.inventoryManager.quickAccessAdd(petList.get(i).getName());
-        GameManager.get().getWorld().removeEntity(petList.get(i));
-        petList.get(i+1).setSummoned(true);
+    /**
+     * Summon other pet
+     * @param i index
+     */
+    private void summonNext(int i,MainCharacter mc){
+        if(i==0){
+            petList.getLast().setSummoned(false);
+            this.inventoryManager.inventoryAdd((Item) petList.getLast());
+            GameManager.get().getWorld().removeEntity(petList.getLast());
+        }else{
+            petList.get(i-1).setSummoned(false);
+            this.inventoryManager.inventoryAdd((Item) petList.get(i-1));
+            GameManager.get().getWorld().removeEntity(petList.get(i-1));
+        }
+        //the new pet appear in the world
+        AbstractPet pet=petList.get(i);
+        pet.setRow(mc.getRow()-1);
+        pet.setCol(mc.getCol()-1);
+        pet.setSummoned(true);
+        pet.setDomesticated(true);
         GameManager.get().getWorld().addEntity(pet);
-    }
-
-    public void feedPet(){
 
     }
 
+    /**
+     * Return the index of pet in the map
+     * @param abstractPet target pet
+     * @return index of pet
+     */
     public int petPositionInList(AbstractPet abstractPet){
         for(int i=0;i<petList.size();i++){
             if(petList.get(i)==abstractPet){
@@ -91,7 +133,12 @@ public class PetsManager extends AbstractManager {
         }
         return 0;
     }
-    
+
+    /**
+     * To check whether that pet is summoned
+     * @param pet
+     * @return
+     */
     public boolean isSummoned(AbstractPet pet){
         if(pet.isSummoned()){
             return true;
@@ -99,9 +146,6 @@ public class PetsManager extends AbstractManager {
         return false;
     }
 
-    public void sellPet(){
-
-    }
 
 
 }
