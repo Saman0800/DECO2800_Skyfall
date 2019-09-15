cd673556c9b2873a0606acca7d903e601f0f5198
Peng Shu <s4518373@student.uq.edu.au>
2019-09-15 13:26:52 +1000

    remove enemy robot and spider

--- src/main/java/deco2800/skyfall/worlds/world/WorldDirector.java
+++ src/main/java/deco2800/skyfall/worlds/world/WorldDirector.java
@@ -63,9 +63,7 @@ public class WorldDirector {
 
         builder.addEntity(mainCharacter);
         GameManager.getManagerFromInstance(GameMenuManager.class).setMainCharacter(mainCharacter);
-
-        builder.addEntity(new Spider(-8f, -1f, mainCharacter));
-        builder.addEntity(new Robot(-4, -2, mainCharacter));
+        
         builder.addEntity(new Stone(-4, -3, mainCharacter));
         builder.addEntity(new Flower(5f,2f,mainCharacter));
         builder.addEntity(new Treeman(-2f,-3f,mainCharacter));
