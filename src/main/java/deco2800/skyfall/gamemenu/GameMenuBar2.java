9ac0f241bf3a57ad3984b3cff76e15ba7326338d
Kausthubram Rajesh <kau23@live.com>
2019-09-24 22:51:45 +1000

    new menu bar

---
+++ src/main/java/deco2800/skyfall/gamemenu/GameMenuBar2.java
@@ -0,0 +1,16 @@
+package deco2800.skyfall.gamemenu;
+
+public class GameMenuBar2 extends AbstractUIElement {
+
+
+
+    @Override
+    public void updatePosition() {
+
+    }
+
+    @Override
+    public void draw() {
+
+    }
+}
