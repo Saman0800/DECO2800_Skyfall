52329ec75fba70af7626874e148fa54563a05dc7
Kathryn Bird <k.bird@uq.net.au>
2019-10-03 20:16:06 +1000

    Fixed lostLevelTest() in statistics manager test

--- src/test/java/deco2800/skyfall/managers/StatisticsManagerTest.java
+++ src/test/java/deco2800/skyfall/managers/StatisticsManagerTest.java
@@ -135,7 +135,7 @@ public class StatisticsManagerTest {
         testManager.loseLevel();
 
         Assert.assertEquals(5, testManager.getLevel());
-        Assert.assertEquals(50, testManager.getHealth());
+        Assert.assertEquals(0, testManager.getHealth());
 
     }
 
