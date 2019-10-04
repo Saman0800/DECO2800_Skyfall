bd972144ad0b584de5241c36437439e90dbaeec9
Gloria CHAN <ch4gloria@gmail.com>
2019-10-04 11:56:29 +1000

    Add some methods and initializations for AbstractVehicle

--- src/main/java/deco2800/skyfall/entities/vehicle/AbstractVehicle.java
+++ src/main/java/deco2800/skyfall/entities/vehicle/AbstractVehicle.java
@@ -1,5 +1,42 @@
 package deco2800.skyfall.entities.vehicle;
 
-public abstract class AbstractVehicle {
-    
+import deco2800.skyfall.entities.ICombatEntity;
+import deco2800.skyfall.entities.Peon;
+
+public abstract class AbstractVehicle extends Peon implements ICombatEntity {
+    private int health;
+    private boolean isTaken = false;
+
+    public AbstractVehicle(float col, float row) {
+        this.setRow(row);
+        this.setCol(col);
+    }
+
+    public AbstractVehicle(float row, float col, String texturename, int health) {
+        super(row, col, 0.2f, texturename, health);
+        this.setTexture(texturename);
+
+    }
+
+    /**
+     * Get the health of the vehicle
+     *
+     * @return health of pet
+     */
+    public int getHealth() {
+        return health;
+    }
+
+    /**
+     * Setting the health to a specific value
+     *
+     * @param health
+     */
+    public void setHealth(int health) {
+        this.health = health;
+    }
+
+    public boolean underUsing(){
+        return isTaken;
+    }
 }
