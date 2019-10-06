b2002a40144d2f4f2deb3dd8f446cd2094eada49
Yu Feng Zheng <s4510976@labs.eait.uq.edu.au>
2019-10-06 18:24:34 +1000

    Added the comment for hot spring class

--- src/main/java/deco2800/skyfall/entities/worlditems/hotSpring.java
+++ src/main/java/deco2800/skyfall/entities/worlditems/hotSpring.java
@@ -7,12 +7,26 @@ import deco2800.skyfall.entities.MainCharacter;
 
 public class hotSpring extends AbstractEntity {
 
+    /**
+     * Create a hot spring in forest biome
+     *
+     */
+    //Entity ID of hot spring
     private static final String ENTITY_ID_STRING = "hot_spring";
+    //Amount of recovering
     private static final int Recover_Healing_power = 3;
+    //Biome
     private static final transient String BIOME = "forest";
+    //MainCharacter player
     private MainCharacter player;
 
-
+    /**
+     * Create a constructor of hot spring
+     *
+     * @param col - the column of hot spring
+     * @param row - the row of hot spring
+     * @param player - the character
+     */
     public hotSpring(float col, float row, MainCharacter player) {
         super(col, row, 2);
         this.setTexture("hot_spring");
@@ -21,7 +35,12 @@ public class hotSpring extends AbstractEntity {
         this.player = player;
     }
 
-
+    /**
+     * Tick of game
+     *
+     * @param i - a long number
+     *
+     */
     public void onTick(long i) {
         if (player != null) {
             float colDistance = player.getCol() - this.getCol();
@@ -41,6 +60,11 @@ public class hotSpring extends AbstractEntity {
         }
     }
 
+    /**
+     * Recovering the player healing power, if player in the hot spring
+     *
+     * @param player - MainCharacter
+     */
     public void playerRecovery(MainCharacter player){
         float colDistance = player.getCol() - this.getCol();
         float rowDistance = player.getRow() - this.getRow();
@@ -52,14 +76,26 @@ public class hotSpring extends AbstractEntity {
 
     }
 
+    /**
+     * The amount of recovering
+     */
     public int amountOfRecovering(){
         return Recover_Healing_power;
     }
+
+
+    /**
+     * Get biome
+     */
     public String getBiome() {
         return BIOME;
     }
 
-
+    /**
+     * Set the boolean recovering to true
+     *
+     * @param player - MainCharacter
+     */
     public void Recovery(MainCharacter player){
         if(!player.isRecovering()){
             player.setRecovering(true);
