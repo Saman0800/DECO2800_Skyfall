abb70f282a34eff10cfd564eeb6f41cd47b7a504
Yu Feng Zheng <s4510976@labs.eait.uq.edu.au>
2019-10-06 19:52:27 +1000

    Added the comment for ruined city class

--- src/main/java/deco2800/skyfall/entities/worlditems/ruinedCity.java
+++ src/main/java/deco2800/skyfall/entities/worlditems/ruinedCity.java
@@ -3,22 +3,49 @@ package deco2800.skyfall.entities.worlditems;
 import deco2800.skyfall.worlds.Tile;
 
 public class ruinedCity extends AbstractEnvironment {
+
+    /**
+     *Entity ID
+     *
+     */
     private static final String ENTITY_ID_STRING = "ruined_city";
 
+    /**
+     *Set the entity ID of ruined city to the object name
+     *
+     */
     public ruinedCity() {
         this.setObjectName(ENTITY_ID_STRING);
     }
 
+    /**
+     *Initialises
+     *
+     *@param tile - The tile it spawns on
+     *@param obstructed - Whether the entity is obstructed by something
+     */
     public ruinedCity(Tile tile, boolean obstructed) {
         super(tile, "ruinedCity", obstructed);
         this.setObjectName(ENTITY_ID_STRING);
         this.entityType = "ruinedCity";
     }
 
+
+
+    /**
+     * Loads a static entity from a memento
+     *
+     * @param memento the static ruined city to add
+     */
     public ruinedCity(SaveableEntityMemento memento) {
         super(memento);
     }
 
+    /**
+     * Initialises ruined city
+     *
+     * @param tile - The tile it spawns on
+     */
     @Override
     public ruinedCity newInstance(Tile tile) {
         return new ruinedCity(tile, this.isObstructed());
--- src/main/java/deco2800/skyfall/entities/worlditems/ruinedRobot.java
+++ src/main/java/deco2800/skyfall/entities/worlditems/ruinedRobot.java
@@ -3,22 +3,48 @@ package deco2800.skyfall.entities.worlditems;
 import deco2800.skyfall.worlds.Tile;
 
 public class ruinedRobot extends AbstractEnvironment {
+
+    /**
+     *Entity ID
+     *
+     */
     private static final String ENTITY_ID_STRING = "ruined_robot";
 
+    /**
+     *Set the entity ID of ruined robot to the object name
+     *
+     */
     public ruinedRobot() {
         this.setObjectName(ENTITY_ID_STRING);
     }
 
+    /**
+     *Initialises
+     *
+     *@param tile - The tile it spawns on
+     *@param obstructed - Whether the entity is obstructed by something
+     */
     public ruinedRobot(Tile tile, boolean obstructed) {
         super(tile, "ruinedCity", obstructed);
         this.setObjectName(ENTITY_ID_STRING);
         this.entityType = "ruinedCity";
     }
 
+
+    /**
+     * Loads a static entity from a memento
+     *
+     * @param memento the static ruined robot to add
+     */
     public ruinedRobot(SaveableEntityMemento memento) {
         super(memento);
     }
 
+    /**
+     * Initialises ruined robot
+     *
+     * @param tile - The tile it spawns on
+     */
     @Override
     public ruinedRobot newInstance(Tile tile) {
         return new ruinedRobot(tile, this.isObstructed());
