78931aa392e84a5399fefb369b8fa698e0bff7b0
cyruschu <chunting.chu@uqconnect.edu.au>
2019-10-16 23:14:21 +1000

    Added tutorial screen

--- src/main/java/deco2800/skyfall/mainmenu/MainMenuScreen.java
+++ src/main/java/deco2800/skyfall/mainmenu/MainMenuScreen.java
@@ -16,13 +16,14 @@ import deco2800.skyfall.managers.DatabaseManager;
 import deco2800.skyfall.managers.GameManager;
 import deco2800.skyfall.managers.TextureManager;
 import deco2800.skyfall.saving.Save;
+import deco2800.skyfall.gamemenu.TutorialScreen;
 
 import java.util.List;
 import java.util.Random;
 
 public class MainMenuScreen implements Screen {
-    public static final String MAIN_MENU_STYLE = "main_menu";
-    public static final String LOAD_GAME_STYLE = "load-game";
+    private static final String MAIN_MENU_STYLE = "main_menu";
+    private static final String LOAD_GAME_STYLE = "load-game";
 
     private final SkyfallGame game;
     private Stage stage;
@@ -31,9 +32,9 @@ public class MainMenuScreen implements Screen {
     private static final int MIN_WIDTH = 1280;
 
     // Used for generating readable save names.
-    private static char[] CONSONANTS =
+    private static final char[] CONSONANTS =
             { 'B', 'C', 'D', 'F', 'G', 'H', 'J', 'K', 'L', 'M', 'N', 'P', 'R', 'S', 'T', 'V', 'W', 'X', 'Z' };
-    private static char[] VOWELS = { 'A', 'E', 'I', 'O', 'U', 'Y' };
+    private static final char[] VOWELS = { 'A', 'E', 'I', 'O', 'U', 'Y' };
 
     private final List<Save> saveInfoList;
     private Window loadGameWindow;
@@ -90,8 +91,8 @@ public class MainMenuScreen implements Screen {
             @Override
             public void clicked(InputEvent event, float x, float y) {
                 GameManager.get().isTutorial = true;
-                // TODO Accept user-provided seed or generate random seed.
-                game.setScreen(new GameScreen(game, 3, true));
+//                game.setScreen(new GameScreen(game, 3, true));
+                game.setScreen(new TutorialScreen());
             }
         });
 
