bee6a03ff7c54f1b6c4c83fb41eb3e9c37fee4c8
kelsey_quinn <s4172433@student.uq.edu.au>
2019-10-17 21:24:10 +1000

    Textures and draw for feedback bar implemented

--- src/main/java/deco2800/skyfall/gamemenu/FeedbackBar.java
+++ src/main/java/deco2800/skyfall/gamemenu/FeedbackBar.java
@@ -3,24 +3,33 @@ package deco2800.skyfall.gamemenu;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import deco2800.skyfall.managers.GameMenuManager;
 import deco2800.skyfall.managers.TextureManager;
 
 public class FeedbackBar extends AbstractUIElement {
 
+    //Game menu manager
+    private final GameMenuManager gmm;
+
     //Current skin
     private final Skin skin;
 
-    //Text in feedback bar
+    //Feedback text
     private Label feedback;
 
-    public FeedbackBar(Stage stage, String[] textureNames, TextureManager tm, Skin skin) {
+    //Feedback bar
+    private Table feedbackBar;
+
+    public FeedbackBar(Stage stage, String[] textureNames, TextureManager tm, Skin skin, GameMenuManager gmm) {
         super(stage, textureNames, tm);
+        this.gmm = gmm;
         this.skin = skin;
     }
 
     @Override
     public void updatePosition() {
-        //to be implemented
+        feedbackBar.setPosition(gmm.getBottomRightX() - 300, gmm.getBottomRightY());
     }
 
     @Override
@@ -30,6 +39,13 @@ public class FeedbackBar extends AbstractUIElement {
 
     @Override
     public void draw() {
-        //to be implemented
+        feedbackBar = new Table();
+        feedbackBar.setBackground(gmm.generateTextureRegionDrawableObject("feedback_bar"));
+        feedbackBar.setSize(500, 65);
+
+        feedback = new Label("", skin, "white-text");
+        feedback.setFontScale(0.8f);
+        feedbackBar.add(feedback).padBottom(10);
+        stage.addActor(feedbackBar);
     }
 }
