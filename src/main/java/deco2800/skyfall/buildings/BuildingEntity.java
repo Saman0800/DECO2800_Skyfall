e2f4ea8325da5f2845ccf2f2e903b18c286f20d2
totaland <james.nguyen868@gmail.com>
2019-10-17 22:08:04 +1000

    remove unused toggleblueprintlearned

--- src/main/java/deco2800/skyfall/buildings/BuildingEntity.java
+++ src/main/java/deco2800/skyfall/buildings/BuildingEntity.java
@@ -491,14 +491,6 @@ public class BuildingEntity extends AbstractEntity implements ICombatEntity {
         return true;
     }
 
-    /**
-     * Returns the number of metal required for the item.
-     *
-     * @return The name of the item
-     */
-    public void toggleBlueprintLearned() {
-        // TODO: This method needs to be implimented
-    }
 
     public void cabinInteract() {
         // Resting at the cabin restores a players health.
