1fcc2e10e8628e4fdaf548690f5c628cb5820f5e
Christopher Poli <chris.luke.poli@gmail.com>
2019-10-19 00:33:19 +1000

    Fix more code smells.

--- src/main/java/deco2800/skyfall/resources/HealthResources.java
+++ src/main/java/deco2800/skyfall/resources/HealthResources.java
@@ -2,7 +2,6 @@ package deco2800.skyfall.resources;
 
 import deco2800.skyfall.entities.AbstractEntity;
 import deco2800.skyfall.entities.MainCharacter;
-import deco2800.skyfall.managers.InventoryManager;
 import deco2800.skyfall.util.HexVector;
 import deco2800.skyfall.worlds.Tile;
 
