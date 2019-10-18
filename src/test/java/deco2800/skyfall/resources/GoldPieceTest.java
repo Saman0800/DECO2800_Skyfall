748736ddbd103427d92584f8c17133be8403bc98
stella_lahanas <s.lahanas@uqconnect.edu.au>
2019-10-18 19:03:31 +1000

    Added further tests to GoldPieceTest to improve code coverage.

--- src/test/java/deco2800/skyfall/resources/GoldPieceTest.java
+++ src/test/java/deco2800/skyfall/resources/GoldPieceTest.java
@@ -1,9 +1,16 @@
 package deco2800.skyfall.resources;
 
+import deco2800.skyfall.entities.SaveableEntity;
+import deco2800.skyfall.entities.worlditems.ForestRock;
+import deco2800.skyfall.util.HexVector;
+import deco2800.skyfall.worlds.Tile;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.util.Random;
+
 import static org.junit.Assert.*;
+import static org.powermock.api.mockito.PowerMockito.whenNew;
 
 public class GoldPieceTest {
 
@@ -12,13 +19,18 @@ public class GoldPieceTest {
     private GoldPiece g10;
     private GoldPiece g50;
     private GoldPiece g100;
+    private GoldPiece testGoldPiece;
+    private String goldPieceText;
 
     @Before
-    public void setUp()  {
+    public void setUp() throws Exception {
         g5 = new GoldPiece(5);
         g10 = new GoldPiece(10);
         g50 = new GoldPiece(50);
         g100 = new GoldPiece(100);
+        goldPieceText = "gold_piece";
+        Random random = new Random(0);
+        whenNew(Random.class).withAnyArguments().thenReturn(random);
     }
 
 
@@ -32,5 +44,60 @@ public class GoldPieceTest {
         assertTrue(g10.getValue().equals(10));
         assertTrue(g50.getValue().equals(50));
         assertTrue(g100.getValue().equals(100));
+    }
+
+
+    @Test
+    public void basicConstructorTest() {
+        testGoldPiece = new GoldPiece(5);
+        assertEquals(testGoldPiece.getPosition(), new HexVector(0.0f, 0.0f));
+        assertEquals(testGoldPiece.getCol(), 0.0f, 0.001f);
+        assertEquals(testGoldPiece.getRow(), 0.0f, 0.001f);
+        assertTrue(testGoldPiece.isObstructed());
+        assertEquals(goldPieceText, testGoldPiece.getObjectName());
+    }
+
+    @Test
+    public void mementoConstructorTest() {
+
+        ForestRock toSave = new ForestRock();
+        SaveableEntity.SaveableEntityMemento testGoldPieceMemento = toSave.save();
+        testGoldPiece = new GoldPiece(testGoldPieceMemento);
+        assertEquals(testGoldPiece.getPosition(), new HexVector(0.0f, 0.0f));
+        assertEquals(2, testGoldPiece.getRenderOrder());
+        assertEquals(testGoldPiece.getCol(), 0.0f, 0.001f);
+        assertEquals(testGoldPiece.getRow(), 0.0f, 0.001f);
+        assertTrue(testGoldPiece.isObstructed());
+    }
+
+    @Test
+    public void tileConstructorTest() {
+
+        Tile testTile = new Tile(null, 0.5f, 0.5f);
+
+        testGoldPiece = new GoldPiece(testTile, false);
+        assertEquals(testGoldPiece.getPosition(), new HexVector(0.5f, 0.5f));
+        assertEquals(2, testGoldPiece.getRenderOrder());
+        assertEquals(testGoldPiece.getCol(), 0.5f, 0.001f);
+        assertEquals(testGoldPiece.getRow(), 0.5f, 0.001f);
+        assertFalse(testGoldPiece.isObstructed());
+        assertEquals(goldPieceText, testGoldPiece.getObjectName());
+        assertEquals("GoldPiece", testGoldPiece.getEntityType());
+    }
+
+    @Test
+    public void newInstanceTest() {
+
+        Tile testTile = new Tile(null, 0.5f, 0.5f);
+
+        testGoldPiece = new GoldPiece(testTile, false);
+        GoldPiece newInstanceGold = testGoldPiece.newInstance(testTile);
+
+        assertEquals(newInstanceGold.getPosition(), testGoldPiece.getPosition());
+        assertEquals(newInstanceGold.getCol(), 0.5f, 0.001f);
+        assertEquals(newInstanceGold.getRow(), 0.5f, 0.001f);
+        assertEquals(newInstanceGold.isObstructed(), testGoldPiece.isObstructed());
+        assertEquals(goldPieceText, newInstanceGold.getObjectName());
+        assertEquals("GoldPiece",newInstanceGold.getEntityType());
     }
 }
