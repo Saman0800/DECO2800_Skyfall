c6130d58e007a2685ba0bcabc16383baf00947cf
s4479623 <ashleighrae.oneill@uqconnect.edu.au>
2019-10-19 15:26:27 +1000

    Returns to home

--- src/main/java/deco2800/skyfall/gamemenu/popupmenu/PauseTable.java
+++ src/main/java/deco2800/skyfall/gamemenu/popupmenu/PauseTable.java
@@ -1,6 +1,8 @@
 package deco2800.skyfall.gamemenu.popupmenu;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.Stage;
@@ -8,10 +10,12 @@ import com.badlogic.gdx.scenes.scene2d.ui.*;
 import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 import com.badlogic.gdx.utils.Align;
+import deco2800.skyfall.GameLauncher;
+import deco2800.skyfall.GameScreen;
+import deco2800.skyfall.SkyfallGame;
 import deco2800.skyfall.gamemenu.AbstractPopUpElement;
-import deco2800.skyfall.managers.GameMenuManager;
-import deco2800.skyfall.managers.SoundManager;
-import deco2800.skyfall.managers.TextureManager;
+import deco2800.skyfall.mainmenu.MainMenuScreen;
+import deco2800.skyfall.managers.*;
 
 
 /**
@@ -19,8 +23,7 @@ import deco2800.skyfall.managers.TextureManager;
  */
 public class PauseTable extends AbstractPopUpElement{
     private Skin skin;
-
-
+    private SkyfallGame game;
 
     /**
      * Constructs a pause baseTable.
@@ -105,7 +108,7 @@ public class PauseTable extends AbstractPopUpElement{
         toHome.addListener(new ClickListener() {
             @Override
             public void clicked(InputEvent event, float x, float y) {
-                // Do nothing for now.
+                returnHome();
             }
         });
 
@@ -130,7 +133,7 @@ public class PauseTable extends AbstractPopUpElement{
         reset.addListener(new ClickListener() {
             @Override
             public void clicked(InputEvent event, float x, float y) {
-                // Do nothing for now.
+                // Do nothing
             }
         });
 
@@ -147,6 +150,13 @@ public class PauseTable extends AbstractPopUpElement{
         stage.addActor(baseTable);
     }
 
+    /**
+     * Returns to the main screen
+     */
+    public void returnHome() {
+        game = GameManager.getManagerFromInstance(GameMenuManager.class).getGame();
+        game.setScreen(new MainMenuScreen(game));
+    }
 
 }
 
