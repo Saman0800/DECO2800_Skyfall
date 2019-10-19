5328c75ce2a832585d8a5289d7d449cf4f2fe0c3
Kausthubram Rajesh <kau23@live.com>
2019-10-19 13:49:43 +1000

    Fixed #573

--- src/main/java/deco2800/skyfall/entities/MainCharacter.java
+++ src/main/java/deco2800/skyfall/entities/MainCharacter.java
@@ -1656,6 +1656,7 @@ public class MainCharacter extends Peon
             break;
         }
 
+        unlocked.add(new ForestPortal(0, 0, 0));
         // for portals
         switch (save.getGameStage()) {
         case 3:
