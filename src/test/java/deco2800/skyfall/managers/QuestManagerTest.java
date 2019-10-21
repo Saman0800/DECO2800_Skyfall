30d5ea4639f5fd151384d335a730427551924731
kelsey_quinn <s4172433@student.uq.edu.au>
2019-10-21 16:41:40 +1000

    removed unused powermock from QuestManagerTest

--- src/test/java/deco2800/skyfall/managers/QuestManagerTest.java
+++ src/test/java/deco2800/skyfall/managers/QuestManagerTest.java
@@ -1,6 +1,5 @@
 package deco2800.skyfall.managers;
 
-import deco2800.skyfall.buildings.BuildingEntity;
 import deco2800.skyfall.buildings.BuildingType;
 import deco2800.skyfall.entities.AbstractEntity;
 import deco2800.skyfall.resources.GoldPiece;
@@ -10,9 +9,6 @@ import deco2800.skyfall.resources.items.Wood;
 import deco2800.skyfall.worlds.world.World;
 import org.junit.Before;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.powermock.core.classloader.annotations.PrepareForTest;
-import org.powermock.modules.junit4.PowerMockRunner;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -22,8 +18,6 @@ import static org.mockito.Matchers.anyInt;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
-@RunWith(PowerMockRunner.class)
-@PrepareForTest({BuildingType.class})
 public class QuestManagerTest {
 
     //Test managers
@@ -33,9 +27,6 @@ public class QuestManagerTest {
     //Mock objects
     private World mockWorld = mock(World.class);
     private List<AbstractEntity> mockEntities = mock(List.class);
-    private BuildingEntity mockCabin = mock(BuildingEntity.class);
-    private BuildingEntity mockWatchTower = mock(BuildingEntity.class);
-    private BuildingType mockBuilding = mock(BuildingType.class);
 
     @Before
     public void setUp() {
