8e376d6e8d80050fd0285ebd6a009b4e781e84e3
kelsey_quinn <s4172433@student.uq.edu.au>
2019-10-21 19:03:53 +1000

    removed unused imports and methods

--- src/main/java/deco2800/skyfall/SkyfallGame.java
+++ src/main/java/deco2800/skyfall/SkyfallGame.java
@@ -2,7 +2,6 @@ package deco2800.skyfall;
 
 import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.files.FileHandle;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import deco2800.skyfall.mainmenu.MainMenuScreen;
--- src/main/java/deco2800/skyfall/entities/enemies/Enemy.java
+++ src/main/java/deco2800/skyfall/entities/enemies/Enemy.java
@@ -467,7 +467,7 @@ public class Enemy extends Peon implements Animatable, ICombatEntity, Tickable {
      * @return strength of the enemy
      */
     public int getStrength() {
-        return strength;
+        return getDamage();
     }
 
     /**
