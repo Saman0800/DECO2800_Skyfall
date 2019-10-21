377c285bf38e58e25c8314b544b68f5d6ffdf4cd
Kathryn Bird <k.bird@uq.net.au>
2019-10-21 16:03:20 +1000

    Fixed inventory full bug

--- src/main/java/deco2800/skyfall/managers/InventoryManager.java
+++ src/main/java/deco2800/skyfall/managers/InventoryManager.java
@@ -299,21 +299,24 @@ public class InventoryManager extends TickableManager implements Serializable {
     }
 
     private boolean processPosition(String name, List<Item> itemsList, List<Tuple> pos) {
-        if (pos.isEmpty()) {
-            positions.put(name, new Tuple(0, 0));
-            inventory.put(name, itemsList);
-            return true;
-        } else {
-            for (int i = 0; i < ROWS; i++) {
-                for (int j = 0; j < COLS; j++) {
-                    if (!pos.contains(new Tuple(j, i))) {
-                        positions.put(name, new Tuple(j, i));
-                        inventory.put(name, itemsList);
-                        return true;
+        if(inventory.size() < 12){
+            if (pos.isEmpty()) {
+                positions.put(name, new Tuple(0, 0));
+                inventory.put(name, itemsList);
+                return true;
+            } else {
+                for (int i = 0; i < ROWS; i++) {
+                    for (int j = 0; j < COLS; j++) {
+                        if (!pos.contains(new Tuple(j, i))) {
+                            positions.put(name, new Tuple(j, i));
+                            inventory.put(name, itemsList);
+                            return true;
+                        }
                     }
                 }
             }
         }
+
         return false;
     }
 
