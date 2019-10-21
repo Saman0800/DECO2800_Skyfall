089024364d1a425313ae852c3da4a539da828538
Kaleb Day <k.day@uqconnect.edu.au>
2019-10-21 16:20:23 +1000

    fixed bug to stop mainmenu bgm after pressing load game

--- src/main/java/deco2800/skyfall/mainmenu/MainMenuScreen.java
+++ src/main/java/deco2800/skyfall/mainmenu/MainMenuScreen.java
@@ -221,6 +221,7 @@ public class MainMenuScreen implements Screen {
             loadSaveButton.addListener(new ClickListener() {
                 @Override
                 public void clicked(InputEvent event, float x, float y) {
+                    music.stop();
                     game.setScreen(new GameScreen(game, saveID));
                 }
             });
